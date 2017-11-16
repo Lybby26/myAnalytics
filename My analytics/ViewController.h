5580e32e818dfa1e9cf663d0322d5a6dbe935ba4
Mac Mini <macmini@macs-mac-mini.local>
2017-11-15 20:38:39 -0600

    Initial Commit

---
+++ My analytics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  My analytics
+//
+//  Created by Mac Mini on 11/15/17.
+//  Copyright © 2017 lybby26. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ My analytics/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  My analytics
+//
+//  Created by Mac Mini on 11/15/17.
+//  Copyright © 2017 lybby26. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
